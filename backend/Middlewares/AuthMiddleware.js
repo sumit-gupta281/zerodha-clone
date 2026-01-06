d4f508e153f916d1f318c161e907493722d0f857
Sumit Kumar Gupta <skg875736@gmail.com>
2026-01-06 21:30:53 +0530

    fix: user schema field mismatch and auth middleware

--- backend/Middlewares/AuthMiddleware.js
+++ backend/Middlewares/AuthMiddleware.js
@@ -1,19 +1,20 @@
 const User = require("../models/UserModel");
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-module.exports.userVerification = (req, res) => {
-  const token = req.cookies.token
-  if (!token) {
-    return res.json({ status: false })
+module.exports.userVerification = async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) return res.json({ status: false });
+
+    jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
+      if (err) return res.json({ status: false });
+
+      const user = await User.findById(data.id);
+      if (!user) return res.json({ status: false });
+
+      return res.json({ status: true, user: user.name });
+    });
+  } catch (err) {
+    return res.json({ status: false });
   }
-  jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
-    if (err) {
-     return res.json({ status: false })
-    } else {
-      const user = await User.findById(data.id)
-      if (user) return res.json({ status: true, user: user.username })
-      else return res.json({ status: false })
-    }
-  })
-}
+};
--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Your name is required"],
+  },
   email: {
     type: String,
     required: [true, "Your email address is required"],
     unique: true,
-  },
-  username: {
-    type: String,
-    required: [true, "Your username is required"],
   },
   password: {
     type: String,
@@ -22,9 +22,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // hash password before saving
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 module.exports = userSchema;
